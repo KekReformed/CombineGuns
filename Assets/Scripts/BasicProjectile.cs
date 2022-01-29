539cd130423cae4b3c8b216f7514aa111b823068
Jack <jackpullen2004@gmail.com>
2022-01-29 19:51:44 +0000

    Added all fire modes and further modularised the code

--- Assets/Scripts/BasicProjectile.cs
+++ Assets/Scripts/BasicProjectile.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class BasicProjectile : MonoBehaviour
 {
     public float bulletSpeed;
+    public string fireTargetName;
     private Rigidbody2D body;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        Vector2 fireTargetPosition = new Vector2(GameObject.Find("FireTarget").transform.position.x, GameObject.Find("FireTarget").transform.position.y);
+        Vector2 fireTargetPosition = new Vector2(GameObject.Find(fireTargetName).transform.position.x, GameObject.Find(fireTargetName).transform.position.y);
         body.velocity = (fireTargetPosition - body.position)*bulletSpeed;
     }
 
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,103 +3,120 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    [SerializeField] private float attackCooldown;
-    [SerializeField] private float burstInterval;
-    [SerializeField] private float recoil;
-    [SerializeField] private float recoilTime;
-    [SerializeField] private float damage;
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private Rigidbody2D playerBody;
-    [SerializeField] private PlayerMovement player;
-    [SerializeField] private bool multishot;
-    [SerializeField] private bool burst;
-    [SerializeField] private bool automatic;
-    
+	[SerializeField] private float attackCooldown;
+	[SerializeField] private float burstInterval;
+	[SerializeField] private float recoil;
+	[SerializeField] private float recoilTime;
+	[SerializeField] private float damage;
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private Rigidbody2D playerBody;
+	[SerializeField] private PlayerMovement player;
+	[SerializeField] private bool multishot;
+	[SerializeField] private bool burst;
+	[SerializeField] private bool automatic;
+	
 
-    private Transform firePoint;
-    private float cooldownTimer;
-    private float recoilTimer;
-    private bool resetAccelerations = true;
+	private Transform firePoint;
+	private float cooldownTimer;
+	private float recoilTimer;
+	private bool resetAccelerations = true;
 
-    private void Awake()
-    {
-        firePoint = transform.Find("FirePoint");
-        if (firePoint == null)
-        {
-            Debug.Log("You need an object called FirePoint!!!");
-        }
-    }
+	private void Awake()
+	{
+		firePoint = transform.Find("FirePoint");
+		if (firePoint == null)
+		{
+			Debug.Log("You need an object called FirePoint!!!");
+		}
+	}
 
-    private void Update()
-    {
-        if (cooldownTimer > attackCooldown)
-        {
-            if (Input.GetMouseButtonDown(0) && automatic == false)
+	private void Update()
+	{
+		if (cooldownTimer > attackCooldown)
+		{
+			if (Input.GetMouseButtonDown(0) && automatic == false)
+			{
+				Shoot(recoil,burst,multishot);
+			}
+
+			if (Input.GetMouseButton(0) && automatic == true)
+			{
+				Shoot(recoil,burst,multishot);
+			}
+		}
+		cooldownTimer += Time.deltaTime;
+		recoilTimer += Time.deltaTime;
+
+		if (recoilTimer > recoilTime && !resetAccelerations)
+		{
+			player.acceleration *= 10f;
+			player.decceleration *= 10f;
+			resetAccelerations = true;
+		}
+
+	}
+	
+	private void Shoot(float recoil,bool burst, bool multishot)
+	{
+		Vector2 fireTargetPosition = new Vector2(GameObject.Find("FireTarget").transform.position.x, GameObject.Find("FireTarget").transform.position.y);
+		playerBody.velocity += (fireTargetPosition - playerBody.position) * -recoil;
+		cooldownTimer = 0;
+		recoilTimer = 0;
+
+		if (resetAccelerations)
+		{
+			player.acceleration *= 0.1f;
+			player.decceleration *= 0.1f;
+			resetAccelerations = false;
+		}
+
+		if (burst)
+		{
+			StartCoroutine(Burst("FireTarget"));
+		}
+
+		if (multishot)
+		{
+			if (burst)
+			{
+				StartCoroutine(Burst("FireTargetShotgunUppist"));
+				StartCoroutine(Burst("FireTargetShotgunUpper"));
+				StartCoroutine(Burst("FireTargetShotgunLower"));
+				StartCoroutine(Burst("FireTargetShotgunLowist"));
+			}
+            else
             {
-                if (!multishot)
-                {
-                    Shoot(recoil,burst,multishot);
-                }
-                else if (multishot)
-                {
-                    Debug.Log("You haven't added that yet dumbo!");
-                }
-            }
+				pew("FireTargetShotgunUppist");
+				pew("FireTargetShotgunUpper");
+				pew("FireTarget");
+				pew("FireTargetShotgunLower");
+				pew("FireTargetShotgunLowist");
+			}
+		}
 
-            if (Input.GetMouseButton(0) && automatic == true)
-            {
-                Shoot(recoil,burst,multishot);
-            }
-        }
-        cooldownTimer += Time.deltaTime;
-        recoilTimer += Time.deltaTime;
+		else
+		{
+			GameObject bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+			bulletClone.GetComponent<BasicProjectile>().fireTargetName = "FireTarget";
+		}
 
-        if (recoilTimer > recoilTime && !resetAccelerations)
-        {
-            player.acceleration *= 10f;
-            player.decceleration *= 10f;
-            resetAccelerations = true;
-        }
+	}
 
-    }
 
-    private void Shoot(float recoil,bool burst, bool multishot)
+	private void pew(string fireTarget)
     {
-        Vector2 fireTargetPosition = new Vector2(GameObject.Find("FireTarget").transform.position.x, GameObject.Find("FireTarget").transform.position.y);
-        playerBody.velocity += (fireTargetPosition - playerBody.position) * -recoil;
-        cooldownTimer = 0;
-        recoilTimer = 0;
+		GameObject bulletClone_1 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		bulletClone_1.GetComponent<BasicProjectile>().fireTargetName = fireTarget;
 
-        if (resetAccelerations)
-        {
-            player.acceleration *= 0.1f;
-            player.decceleration *= 0.1f;
-            resetAccelerations = false;
-        }
+	}
 
-        if (burst)
-        {
-            StartCoroutine(Burst(multishot));
-        }
-
-        if (multishot)
-        {
-
-        }
-
-        else
-        {
-            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        }
-
-    }
-
-    IEnumerator Burst(bool multishot)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-            yield return new WaitForSeconds(burstInterval);
-        }
-    }
+	IEnumerator Burst(string targetName)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			GameObject bulletClone = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+			bulletClone.GetComponent<BasicProjectile>().fireTargetName = targetName;
+			yield return new WaitForSeconds(burstInterval);
+		}
+	}
 }
